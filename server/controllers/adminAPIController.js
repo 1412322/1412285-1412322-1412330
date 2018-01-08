16736b0dd4e6980d3f17665bbab277e21ca1b24f
Asus\Pig My <pigmy96@gmail.com>
2018-01-08 10:49:48 +0700

    update 4.4: chỉ hiện thông tin của hệ thống chứ ko hiển thị toàn bộ

--- server/controllers/adminAPIController.js
+++ server/controllers/adminAPIController.js
@@ -143,68 +143,198 @@ getBlocks = function(res)
         if (!transList) {
           res.json({ success: false, msg: 'Transaction is empty!' });
         } else {
-          var listResult = [];
-          for (let i = 0; i < blockList.length; i++)
-          {
-            var time = blockList[i].timestamp;
-            var date = new Date(time * 1000);
-            var hours = date.getHours();
-            var minutes = "0" + date.getMinutes();
-            var seconds = "0" + date.getSeconds();
-            var day = date.getDate() + '/' + (date.getMonth()+1) + '/' + date.getFullYear();
-            var formattedDate = day + ' ' + hours + ':' + minutes.substr(-2) + ':' + seconds.substr(-2);
-            for (let j = 0; j < blockList[i].transactions.length; j++)
+          User.find(function(err3,userList){
+            if (err3)
             {
-              for (let k = 0; k < transList.length; k++)
-              {
-                if (transList[k].hash == blockList[i].transactions[j])
-                {
-                  var transDetail = transList[k];
-                  var inputs = [];
-                  for (let l = 0; l < transDetail.inputs.length; l++)
-                  {
-                    var unlockScript = transDetail.inputs[l].unlockScript.split(' ');
-                    if (unlockScript[0] == 'PUB' && unlockScript[2] == 'SIG')
-                    {
-                      var input =
-                      {
-                        referencedOutputHash: transDetail.inputs[l].referencedOutputHash,
-                        referencedOutputIndex: transDetail.inputs[l].referencedOutputIndex
-                      };
-                      inputs.push(input);
-                    }
-                  }
-                  if (inputs.length > 0)
-                  {
-                    var outputs = [];
-                    for (let l = 0; l < transDetail.outputs.length; l++)
-                    {
-                      var lockScript = transDetail.outputs[l].lockScript.split(' ');
-                      var output =
-                      {
-                        value: transDetail.outputs[l].value,
-                        address: lockScript[1],
-                        index: l
-                      };
-                      outputs.push(output);
-                    }
-                    var transInfo = {
-                      hash: transList[k].hash,
-                      time: formattedDate,
-                      state: transList[k].state,
-                      inputs: inputs,//tham chiếu output index
-                      outputs: outputs
-                    };
-                    listResult.push(transInfo);
-                  }
-
-                }
-              }
+              res.json({ success: false, msg: 'Get User List Failed!', error: err3 });
             }
+            if (!userList) {
+              res.json({ success: false, msg: ' User List is empty!' });
+            } else {
+              var listResult = [];
+              for (let i = 0; i < blockList.length; i++)
+              {
+                var time = blockList[i].timestamp;
+                var date = new Date(time * 1000);
+                var hours = date.getHours();
+                var minutes = "0" + date.getMinutes();
+                var seconds = "0" + date.getSeconds();
+                var day = date.getDate() + '/' + (date.getMonth()+1) + '/' + date.getFullYear();
+                var formattedDate = day + ' ' + hours + ':' + minutes.substr(-2) + ':' + seconds.substr(-2);
+                for (let j = 0; j < blockList[i].transactions.length; j++)
+                {
+                  for (let k = 0; k < transList.length; k++)
+                  {
+                    if (transList[k].hash == blockList[i].transactions[j])
+                    {
+                      var transDetail = transList[k];
+                      //var inputs = [];
+                      for (let m = 0; m < transDetail.outputs.length; m++)
+                      {
+                        var lockScript  = transDetail.outputs[m].lockScript.split(' ');
+                        var isSend = false;
+                        var isReceive = false;
+                        var userInfo = '';
+                        for (let n = 0; n < userList.length; n++)
+                        {
+                          if (lockScript[1] == userList[n].address)//địa chỉ của user có trong output
+                          {
+                            if (m == 0)//user đó chính là người gửi tiền, đây là địa chỉ tiền thối
+                            {
+                              isSend = true;
+                              isReceive = false;
+                              userInfo = userList[n];
+                              break;
+                            }
+                            else// user ko gửi, chỉ nhận
+                            {
+                              isSend = false;
+                              isReceive = true;
+                              userInfo = userList[n];
+                              break;
+                            }
+                          }
+                        }
+                        if (isSend == true)
+                        {
+                          var outputs = [];
+                          var inputs = [];
+                          for (let l = 0; l < transDetail.outputs.length; l++)
+                          {
+                            var lockScript = transDetail.outputs[l].lockScript.split(' ');
+                            if (lockScript[1] == userInfo.address)
+                            {
+                              var output =
+                              {
+                                value: transDetail.outputs[l].value,
+                                address: lockScript[1],
+                                index: l,
+                                userInfo: userInfo.email
+                              };
+                              outputs.push(output);
+                            }
+                            else
+                            {
+                              var output =
+                              {
+                                value: transDetail.outputs[l].value,
+                                address: lockScript[1],
+                                index: l,
+                                userInfo: null
+                              };
+                              outputs.push(output);
+                            }
 
-          }
-          res.json({success: true,
-            listResult: listResult });
+                          }
+
+                          for (let l = 0; l < transDetail.inputs.length; l++)
+                          {
+                            var input =
+                            {
+                              referencedOutputHash: transDetail.inputs[l].referencedOutputHash,
+                              referencedOutputIndex: transDetail.inputs[l].referencedOutputIndex
+                            };
+                            inputs.push(input);
+                          }
+                          var transInfo = {
+                            hash: transList[k].hash,
+                            time: formattedDate,
+                            state: transList[k].state,
+                            inputs: inputs,//tham chiếu output index
+                            outputs: outputs
+                          };
+                          listResult.push(transInfo);
+                          //break;
+                        }
+                        if (isReceive == true)
+                        {
+                          var outputs = [];
+                          var inputs = [];
+                          for (let l = 0; l < transDetail.outputs.length; l++)
+                          {
+                            var lockScript = transDetail.outputs[l].lockScript.split(' ');
+                            if (lockScript[1] == userInfo.address)
+                            {
+                              var output =
+                              {
+                                value: transDetail.outputs[l].value,
+                                address: lockScript[1],
+                                index: l,
+                                userInfo: userInfo.email
+                              };
+                              outputs.push(output);
+                            }
+
+                          }
+
+                          for (let l = 0; l < transDetail.inputs.length; l++)
+                          {
+                            var input =
+                            {
+                              referencedOutputHash: transDetail.inputs[l].referencedOutputHash,
+                              referencedOutputIndex: transDetail.inputs[l].referencedOutputIndex
+                            };
+                            inputs.push(input);
+                          }
+                          var transInfo = {
+                            hash: transList[k].hash,
+                            time: formattedDate,
+                            state: transList[k].state,
+                            inputs: inputs,//tham chiếu output index
+                            outputs: outputs
+                          };
+                          listResult.push(transInfo);
+                          //break;
+                        }
+                      }
+                      //
+                      /*for (let l = 0; l < transDetail.inputs.length; l++)
+                      {
+                        var unlockScript = transDetail.inputs[l].unlockScript.split(' ');
+                        if (unlockScript[0] == 'PUB' && unlockScript[2] == 'SIG')
+                        {
+                          var input =
+                          {
+                            referencedOutputHash: transDetail.inputs[l].referencedOutputHash,
+                            referencedOutputIndex: transDetail.inputs[l].referencedOutputIndex
+                          };
+                          inputs.push(input);
+                        }
+                      }
+                      if (inputs.length > 0)
+                      {
+                        var outputs = [];
+                        for (let l = 0; l < transDetail.outputs.length; l++)
+                        {
+                          var lockScript = transDetail.outputs[l].lockScript.split(' ');
+                          var output =
+                          {
+                            value: transDetail.outputs[l].value,
+                            address: lockScript[1],
+                            index: l
+                          };
+                          outputs.push(output);
+                        }
+                        var transInfo = {
+                          hash: transList[k].hash,
+                          time: formattedDate,
+                          state: transList[k].state,
+                          inputs: inputs,//tham chiếu output index
+                          outputs: outputs
+                        };
+                        listResult.push(transInfo);
+                      }*/
+
+                    }
+                  }
+                }
+
+              }
+              res.json({success: true,
+                listResult: listResult });
+            }
+          });
+
         }
       });
     }
